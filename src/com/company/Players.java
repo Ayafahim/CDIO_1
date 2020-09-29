0ed41071fc4b3ced8464c4da83a34eb0d07ee5a6
Aya Fahim <bvf-2002@live.dk>
2020-09-29 14:28:35 +0200

    players class

---
+++ src/com/company/Players.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Players {
+    String name;
+    int points;
+
+
+    //constructor
+
+    Players(String n, int p){
+        name = n;
+        points = p;
+    }
+
+
+
+
+}
