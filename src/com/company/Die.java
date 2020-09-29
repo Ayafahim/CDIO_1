56b98d416541718cacf82589be225cfcee37a3e0
Aya Fahim <bvf-2002@live.dk>
2020-09-29 14:11:47 +0200

    første commit

---
+++ src/com/company/Die.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Die {
+
+    int die1 = (int) (Math.random() * (6-1 + 1)+1);
+    int die2 = (int) (Math.random() * (6-1 + 1)+1);
+
+
+   
+
+
+
+
+
+
+
+    
+
+
+}
