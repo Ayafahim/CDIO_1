833e60927748f6ff0e254ac531573f53de4efd2a
Aya Fahim <bvf-2002@live.dk>
2020-09-30 10:59:36 +0200

    Man kan nu rulle en gang, men der er stadig problemer med at vise summen af terningerne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner PlayerName = new Scanner(System.in);
+        Scanner play = new Scanner(System.in);
         String playername1, playername2;
 
         System.out.print("Player 1's name: ");
@@ -18,6 +19,26 @@ public class Main {
 
         System.out.println("Hej " + playername1 + " du har " + player1.points + " point");
         System.out.println("Hej " + playername2 + " du har " + player2.points + " point");
+
+        play.next();
+
+        Die die1 = new Die(1);
+        Die die2 = new Die(1);
+
+        die1.rollDice();
+        die2.rollDice();
+
+
+
+        System.out.println(playername1 + " rolled " + die1 + " & " + die2 + ". Sum of points added: " );
+
+
+
+
+
+
+
+
 
 
 
