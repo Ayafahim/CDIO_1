380823192aa1ada0bfd6b4d7e3ff84b647a17a17
Aya Fahim <bvf-2002@live.dk>
2020-09-30 14:10:31 +0200

    While lykker og if-Statements tilføjet for at kunne få terninger til at kaste om og om igen

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -29,6 +29,48 @@ public class Main {
 
         gui.setDice(1, 1);
 
+        //input fra spillerne
+
+        while(gui_Player1.getBalance() < 40 || gui_Player2.getBalance() < 40) {
+
+            String knapString = gui.getUserSelection("Kast terning for", "Terning");
+
+            if (knapString == spiller1 && gui_Player1.getBalance() < 40) {
+
+                gui.setDice(4,4);
+
+
+            }
+
+            else if(knapString == spiller2 && gui_Player2.getBalance() < 40){
+
+                gui.setDice(5,5);
+
+            }
+
+            else{
+
+                if (gui_Player1.getBalance() >= 40){
+                    System.out.println(spiller1 + " har vundet!");
+                }
+                else {
+                    System.out.println(spiller2 + " har vundet!");
+                }
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
 
         Die dice = new Die();
         dice.roleDice();
